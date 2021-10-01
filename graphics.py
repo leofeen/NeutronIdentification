ab571054eb92e43ef0641b0ca11608ccb3107597
Leofeen <leofeen@yandex.ru>
2021-10-01 19:32:26 +0300

    Modified graphics

--- graphics.py
+++ graphics.py
@@ -2,18 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 oys = [] # значения на oy
+labels = []
 
 with open('data_w_source.txt') as data_file:
     data = data_file.readlines()
     for i in range(0, len(data)):
         oys.append(list(map(float, data[i].split())))
 
+with open('class_labels.txt') as data_file:
+    data = data_file.readlines()
+    for i in range(0, len(data)):
+        labels.append(int(data[i]))
+
 
 ox = np.arange(0, 3735, 1)
 
 plt.figure(figsize=(20, 16))
-plt.subplot(211)
-plt.plot(ox, oys[0])
-plt.subplot(212)
-plt.plot(ox, oys[2])
+
+j = 1
+for data, label in zip(oys, labels):
+    if label == 1:
+        plt.subplot(3, 3, j)
+        plt.plot(ox, data)
+        j += 1
+        if j == 10:
+            break
+
 plt.show()
