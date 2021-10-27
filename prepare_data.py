7aa67a210b1a75eeee6e26c64d35d7676ccae682
alekela <76111160+alekela@users.noreply.github.com>
2021-10-27 08:33:39 +0300

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,20 +1,13 @@
-# класс, который преобразует данные из тектового файла во вложенный список
-
-
 class Data:
+    """класс, который преобразует данные из тектового файла во вложенный список"""
     def __init__(self, name):
-        self.set_file(name)
-        f = open(self.file)
-        lines = f.read().split('\n')
-        for i in range(len(lines)):
-            lines[i] = list(map(float, lines[i].split()))
-        self.d = lines.copy()
+        with open(name) as f:
+            self.d = f.readlines()
+            for i in range(len(self.d)):
+                self.d[i] = list(map(float, self.d[i].split()))
 
     def get_data(self):
         return self.d
-
-    def set_file(self, name):
-        self.file = f'{name}'
 
 
 """data = Data('data_w_source.txt').get_data()
