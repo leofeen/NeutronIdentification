07b2c03fc1aec9a60dafa797fe71a6af3b07ec25
alekela <76111160+alekela@users.noreply.github.com>
2021-11-04 21:22:58 +0300

    Upgraded neural network

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from prepare_data import Data
 from neural_network import OurNeuralNetwork
 
-inputdata = Data('data_w_source.txt', "class_labels.txt")
+"""inputdata = Data('data_w_source.txt', "class_labels.txt")
 data = inputdata.get_data()
-answers = inputdata.get_answers()
+answers = inputdata.get_answers()"""
+
+data = [[-2, -1], [25, 6], [17, 4], [-15, -6]]
+answers = [0, 1, 1, 0]
 
 # создание нейронки
-network = OurNeuralNetwork(4, 3735, 100, 10, 1)
+network = OurNeuralNetwork(3, 2, 2, 1)
+
+# при самом первом запуске нейронки создать файл weights.txt и запустить эту строку
+# network.create_new_weights()
+
+network.train(data, answers)
+
 with open("weights.txt", "w") as f:
     for i in network.weights:
         for j in i:
             f.write(" ".join(map(str, j)) + "\n")
-
-network.train(data, answers)
-
-with open("weights2.txt", "w") as f:
-    for i in network.weights:
-        for j in i:
-            f.write(" ".join(map(str, j)) + "\n")
+        f.write("__\n")
 
 for i in data:
     print(network.feedforward(i))
--- neural_network.py
+++ neural_network.py
@@ -21,7 +21,7 @@ def mse_loss(y_true, y_pred):
 class Neuron:
     def __init__(self, lay, pos):
         self.amount = 0
-        self.value = 0
+        self.value = sigmoid(self.amount)
         self.lay_number = lay
         self.number = pos
         self.bias = 0
@@ -51,13 +51,12 @@ class FirstNeuron(Neuron):
 
 
 class Layer:
-    def __init__(self, number_of_layer, number_of_neurons, previous_layer):
+    def __init__(self, number_of_layer, number_of_neurons):
         self.neurons = [Neuron(number_of_layer, q) for q in range(number_of_neurons)]
-        self.previous_layer = previous_layer
 
-    def count_values(self, weights):
+    def count_values(self, weights, previous_layer):
         for q in range(len(self.neurons)):
-            self.neurons[q].count_value(weights[q], self.previous_layer)
+            self.neurons[q].count_value(weights[q], previous_layer)
 
     def __len__(self):
         return len(self.neurons)
@@ -67,7 +66,7 @@ class FirstLayer(Layer):
     def __init__(self, number_of_layer, number_of_neurons, data):
         self.neurons = [FirstNeuron(0, q, data[q]) for q in range(number_of_neurons)]
 
-    def count_values(self, weights):
+    def count_values(self, weights, previous_layer):
         pass
 
 
@@ -77,58 +76,58 @@ class OurNeuralNetwork:
         """инициализация нейронки, количество слоёв и количество нейронов в каждом слое"""
         self.layers = [FirstLayer(0, number_of_neurons_in_layers[0], [0] * number_of_neurons_in_layers[0])]
         for i in range(1, num_of_layers):
-            self.layers.append(Layer(i, number_of_neurons_in_layers[i], self.layers[-1]))
+            self.layers.append(Layer(i, number_of_neurons_in_layers[i]))
 
-        # создание случайных весов, которые потом поменяются
+        self.weights = []
+        with open("weights.txt") as f:
+            data = f.read().split("__\n")[:-1]
+            for layer_weights in data:
+                layer_weights = layer_weights.split('\n')[:-1]
+                for j in range(len(layer_weights)):
+                    layer_weights[j] = list(map(float, layer_weights[j].split()))
+                self.weights.append(layer_weights)
+
+    def create_new_weights(self):
         self.weights = [[]]
         for i in range(1, len(self.layers)):
             self.weights.append(
-                [[random() for _ in range(number_of_neurons_in_layers[i - 1])] for _ in
-                 range(number_of_neurons_in_layers[i])])
+                [[random() for _ in range(len(self.layers[i - 1]))] for _ in
+                 range(len(self.layers[i]))])
 
     # просто функция, закидываешь данные, возвращается ответ
     def feedforward(self, data):
         if len(self.layers[0]) != len(data):
             raise Exception("Количество данных должно совпадать с количеством нейронов первого слоя!")
         self.layers[0] = FirstLayer(0, len(data), data)
-        for q in range(len(self.layers)):
-            self.layers[q].count_values(self.weights[q])
+        for q in range(1, len(self.layers)):
+            self.layers[q].count_values(self.weights[q], self.layers[q - 1])
         return self.layers[-1].neurons[0].value
+
+    def get_tmp(self):
+        tmp = []
+        for i in self.layers:
+            for j in i.neurons:
+                tmp.append(j.value)
+        return tmp
 
     # функция пока не переделана!!!
     def train(self, data, right_answers):
-        """
-        - data is a (n x 2) numpy array, n = # of samples in the dataset.
-        - all_y_trues is a numpy array with n elements.
-            Elements in all_y_trues correspond to those in data.
-        """
-        learn_rate = 0.5
-        epochs = 50  # количество циклов во всём наборе данных
+        learn_rate = 0.01
+        epochs = 100  # количество циклов во всём наборе данных
 
         for epoch in range(epochs):
 
             for x, y_true in zip(data, right_answers):
-
-                """"# --- Выполняем обратную связь (нам понадобятся эти значения в дальнейшем)
-                neurons = [[] * len(self.layers)]
-                neurons[0] = [(self.layers[0].neurons[i].value, self.layers[0].neurons[i].amount) for i in
-                              range(len(self.layers[0]))]
-                for i in range(1, len(self.layers)):
-                    for j in range(len(self.layers[i])):
-                        neurons[i][j] = (self.layers[i].neurons[j].count_value(self.weights[i][j], self.layers[i - 1]),
-                                         self.layers[i].neurons[j].amount)
-
-                y_pred = neurons[-1][0][0]"""
 
                 y_pred = self.feedforward(x)
 
                 dL_dypred = -2 * (y_true - y_pred)
 
                 for layer in self.layers[1:]:
-                    dw = learn_rate * dL_dypred
+                    dn = learn_rate * dL_dypred
                     for neuron in layer.neurons:
                         num_of_lay = neuron.lay_number
-                        dw *= deriv_sigmoid(neuron.value)
+                        dw = dn * deriv_sigmoid(neuron.value)
                         for num_weight in range(len(neuron.weights)):
                             neuron.weights[num_weight] -= dw * self.layers[num_of_lay - 1].neurons[num_weight].amount
 
@@ -193,31 +192,6 @@ class OurNeuralNetwork:
                 y_preds = np.apply_along_axis(self.feedforward, 1, data)
                 loss = mse_loss(all_y_trues, y_preds)"""
 
-
-""" # Определение набора данных
-data = np.array([
-    [-2, -1],  # Alice
-    [25, 6],  # Bob
-    [17, 4],  # Charlie
-    [-15, -6]
-
-])
-
-all_y_trues = np.array([
-    0,  # Alice
-    1,  # Bob
-    1,  # Charlie
-    0,  # Diana
-])
-
-# Тренируем нашу нейронную сеть!
-network = OurNeuralNetwork()
-network.train(data, all_y_trues)
-O = []
-for i in range(len(data)):
-    O.append(network.feedforward(data[i]))
-output = np.array(O).T
-print(output)"""
 
 """
 network = OurNeuralNetwork(3, 4, 5, 1)
